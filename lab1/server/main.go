7f4d5af7d648d39aec3df5b169d9dbd4bff7269e
Long Cheng <crcleativity@gmail.com>
2023-11-07 16:17:36 +0100

    support request loading and method checking

--- lab1/server/main.go
+++ lab1/server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"net/http"
 	"os"
 )
 
@@ -40,6 +42,33 @@ func main() {
 }
 
 func requestHandler(conn net.Conn) {
+	// echo -e "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n" | nc localhost 8083
+	// echo -e "POST /somepath HTTP/1.1\r\nHost: localhost\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 11\r\n\r\nhello=world" | nc localhost 8083
 	defer conn.Close()
 	fmt.Println("Handling request...")
+
+	// init one buffer, and read info from the connection
+	br := bufio.NewReaderSize(conn, 50*1024*1024) // 50MB buffer
+	request, err := http.ReadRequest(br)
+	if err != nil {
+		fmt.Println("Error listening:", err)
+		os.Exit(1)
+	}
+	// debug, now the basic connection is handled, then specify GET and POST handler for different request
+	fmt.Println("Type of connection request: ", request.Method)
+	switch request.Method {
+	case "GET":
+		getHandler(request)
+	case "POST":
+		postHandler(request)
+	}
+
+}
+
+func getHandler(request *http.Request) {
+	fmt.Println("Handler for get message")
+}
+
+func postHandler(request *http.Request) {
+	fmt.Println("Handler for post message")
 }
